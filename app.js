d89157ede6dc36aacccf479b03131dbbd8d713ad
Trần Văn Lộc <tranvanloc96lhp@gmail.com>
2023-09-05 16:18:37 +0700

    up

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var hbs = require('hbs'); // Định nghĩa thư viện Handlebars
+// const mysql = require('mysql2');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -39,5 +40,7 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,60 @@
 var express = require('express');
 var router = express.Router();
+const Sequelize = require('sequelize');
+
+// Tạo một đối tượng Sequelize và cấu hình kết nối đến cơ sở dữ liệu MySQL
+const sequelize = new Sequelize('testnodejs', 'root', null, {
+  host: 'localhost',
+  dialect: 'mysql'
+});
+
+let connectDB = async () => {
+  try{
+    await sequelize.authenticate();
+    console.log('Kết nối thành công');
+
+  }catch (error) {
+    console.error('Kết nối lỗi', error);
+  }
+}
+
+module.exports = connectDB;
+
+// Định nghĩa model cho bảng city
+const City = sequelize.define('city', {
+  idcity: {
+    type: Sequelize.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  namecity: Sequelize.TEXT,
+  idcountry: Sequelize.INTEGER,
+  imagecity: Sequelize.TEXT
+}
+, {
+  tableName: 'city' // Đặt tên bảng cụ thể là 'city'
+});
+
+
+// Kết nối đến cơ sở dữ liệu
+
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   const cssLink = '<link rel="stylesheet" href="/stylesheets/homepage.css">'; // Mã HTML cho thẻ <link>
 
-  res.render('homepage', { link: cssLink, title: 'LuxStay' });
+  City.findAll()
+  .then(cities => {
+    res.render('homepage', { link: cssLink, title: 'LuxStay',cities }); // Truyền dữ liệu vào template HBS
+  })
+  .catch(error => {
+    console.error('Lỗi truy vấn:', error);
+    res.render('error', { error: error.message });
+    
+  });
+
+
+  // res.render('homepage', { link: cssLink, title: 'LuxStay' });
 });
 
 module.exports = router;
