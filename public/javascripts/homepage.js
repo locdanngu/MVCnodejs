2241172e56a62fdb9644053b7c31a6d81e5f23ae
Trần Văn Lộc <tranvanloc96lhp@gmail.com>
2023-09-06 12:01:52 +0700

    up

--- public/javascripts/homepage.js
+++ public/javascripts/homepage.js
@@ -141,16 +141,3 @@ $(document).ready(function () {
     // 
 });
 
-// Move on Top
-$(window).scroll(function () {
-    if ($(this).scrollTop() >= 300) {
-        $('.move-on-top-btn').fadeIn();
-    }
-    else {
-        $('.move-on-top-btn').fadeOut();
-    }
-});
-
-$('.move-on-top-btn').click(function () {
-    $('html, body').animate({ scrollTop: 0 }, 1500);
-});
--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -7,6 +7,20 @@ $('#searchcity').blur(function () {
     $('#ketquainput').fadeOut();
 });
 
+// Move on Top
+$(window).scroll(function () {
+    if ($(this).scrollTop() >= 300) {
+        $('.move-on-top-btn').fadeIn();
+    }
+    else {
+        $('.move-on-top-btn').fadeOut();
+    }
+});
+
+$('.move-on-top-btn').click(function () {
+    $('html, body').animate({ scrollTop: 0 }, 1500);
+});
+
 
 $(document).ready(function () {
     // Show Nav Mobile
