5aee990ac93f36988aafa0c6c996e2ea9700013e
Trần Văn Lộc <tranvanloc96lhp@gmail.com>
2023-09-06 17:30:03 +0700

    up

--- public/javascripts/homestay.js
+++ public/javascripts/homestay.js
@@ -6,12 +6,54 @@ $('#searchcity2').blur(function () {
     $('#ketquainput2').fadeOut();
 });
 
-$('#ketquainput2').on('click', 'li', function() {
+$('#ketquainput2').on('click', 'li', function () {
     // Lấy tên thành phố từ thẻ p có class "search__suggest-title" trong phần tử li được nhấn
     var cityName = $(this).find('.search__suggest-title').text();
-    
+
     // Gán tên thành phố vào value của input có id "searchcity2"
     $('#searchcity2').val(cityName);
+    // var originalHTML = $('#changecontent').html(); // Lưu nội dung ban đầu
+
+    $('#changecontent').css({
+        'min-height': '500px',
+        'display': 'flex',
+        'justify-content': 'center',
+        'align-items': 'center'
+    });
+
+    // Tạo đoạn mã HTML mới
+    var newHTML = `
+            <div id="wifi-loader">
+                <svg class="circle-outer" viewBox="0 0 86 86">
+                    <circle class="back" cx="43" cy="43" r="40"></circle>
+                    <circle class="front" cx="43" cy="43" r="40"></circle>
+                    <circle class="new" cx="43" cy="43" r="40"></circle>
+                </svg>
+                <svg class="circle-middle" viewBox="0 0 60 60">
+                    <circle class="back" cx="30" cy="30" r="27"></circle>
+                    <circle class="front" cx="30" cy="30" r="27"></circle>
+                </svg>
+                <svg class="circle-inner" viewBox="0 0 34 34">
+                    <circle class="back" cx="17" cy="17" r="14"></circle>
+                    <circle class="front" cx="17" cy="17" r="14"></circle>
+                </svg>
+                <div class="text" data-text="Đang Tìm"></div>
+            </div>
+        `;
+
+    // Thay đổi nội dung của div có id "changecontent" sau 2 giây
+    $('#changecontent').html(newHTML);
+    // setTimeout(function () {
+    //     // Sau 2 giây, quay lại nội dung ban đầu
+    //     $('#changecontent').html(originalHTML);
+    //     $('#changecontent').css({
+    //         'min-height': '',
+    //         'display': '',
+    //         'justify-content': '',
+    //         'align-items': ''
+    //     });
+    //     $('#changecontent').show(); // Hiển thị nội dung ban đầu
+    // }, 2500);
 });
 
 $('#searchcity2').on('input click', function () {
