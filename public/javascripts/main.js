2ad6c111b9f6d96cf8d860e17bd2949ecc62ee99
Trần Văn Lộc <tranvanloc96lhp@gmail.com>
2023-09-06 09:13:23 +0700

    up

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -8,25 +8,25 @@ $('.header__search-input').blur(function () {
 });
 
 
-$(document).ready(function() {
+$(document).ready(function () {
     // Show Nav Mobile
-    $('.nav__mobile--show-btn').click(function() {
+    $('.nav__mobile--show-btn').click(function () {
         $('.nav__mobile').addClass('show-nav__mobile');
         $('.nav_overlay').addClass('show-overlay');
     });
 
-    $('.nav__mobile-close, .nav_overlay').click(function() {
+    $('.nav__mobile-close, .nav_overlay').click(function () {
         $('.nav__mobile').removeClass('show-nav__mobile');
         $('.nav_overlay').removeClass('show-overlay');
     });
 
     // Show Change Languages
-    $('.header__nav-language').click(function() {
+    $('.header__nav-language').click(function () {
         $('.change__languages').toggleClass('show');
     });
 
     // Show Mobile Languages
-    $('.nav__mobile-language').click(function() {
+    $('.nav__mobile-language').click(function () {
         $('.mobile__language').toggleClass('show-language');
     });
 });
@@ -34,4 +34,39 @@ $(document).ready(function() {
 // Loader Page
 $(window).on('load', function (e) {
     $('.loader').delay(1000).fadeOut('lows');
+});
+
+
+$('#searchcity').on('input click', function () {
+    const searchValue = $(this).val();
+
+    $.ajax({
+        url: '/search', // Địa chỉ URL của tệp route hoặc API trên máy chủ Node.js
+        method: 'GET',
+        data: { q: searchValue },
+        success: function (data) {
+            // Xử lý dữ liệu trả về và hiển thị trên giao diện
+            $('#ketquatimkiem').empty();
+            let htmlResult = ''; // Khởi tạo biến để tích luỹ chuỗi HTML
+
+            data.forEach(function (cities) {
+                // Tạo chuỗi HTML cho mỗi thành phố và nối vào biến htmlResult
+                htmlResult += '<li class="search__suggest-item">';
+                htmlResult += '<div class="search__suggest-thumbnail">';
+                htmlResult += '<img src="' + cities.imagecity + '" alt="" class="search__suggest-thumbnail-img">';
+                htmlResult += '</div>';
+                htmlResult += '<div class="search__suggest-content">';
+                htmlResult += '<p class="search__suggest-title">' + cities.namecity + '</p>';
+                htmlResult += '<small>6 Chỗ ở</small>';
+                htmlResult += '</div>';
+                htmlResult += '</li>';
+            });
+
+            // In chuỗi HTML ra searchResults
+            $('#ketquatimkiem').html(htmlResult);
+        },
+        error: function (error) {
+            console.error(error);
+        },
+    });
 });
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 const City = require('../models/City');
+const { Op } = require('sequelize');
+
 
 // let connectDB = async () => {
 //   try{
@@ -11,27 +13,6 @@ const City = require('../models/City');
 //     console.error('Kết nối lỗi', error);
 //   }
 // }
-
-// module.exports = connectDB;
-
-// Định nghĩa model cho bảng city
-// const City = sequelize.define('city', {
-//   idcity: {
-//     type: Sequelize.INTEGER,
-//     primaryKey: true,
-//     autoIncrement: true
-//   },
-//   namecity: Sequelize.TEXT,
-//   idcountry: Sequelize.INTEGER,
-//   imagecity: Sequelize.TEXT
-// }
-// , {
-//   tableName: 'city' // Đặt tên bảng cụ thể là 'city'
-// });
-
-
-// Kết nối đến cơ sở dữ liệu
-
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
@@ -44,11 +25,36 @@ router.get('/', function (req, res, next) {
   .catch(error => {
     console.error('Lỗi truy vấn:', error);
     res.render('error', { error: error.message });
-    
   });
-
-
-  // res.render('homepage', { link: cssLink, title: 'LuxStay' });
 });
+
+router.get('/search', async (req, res) => {
+  const searchQuery = req.query.q;
+  let cities;
+
+  try {
+    if(searchQuery != ''){
+      cities = await City.findAll({
+        where: {
+          namecity: {
+            [Op.like]: `%${searchQuery}%`,
+          },
+        },
+        limit: 6, // Giới hạn số lượng kết quả trả về thành 6
+      });
+    }else if(searchQuery == '' || searchQuery == null){
+      cities = await City.findAll({
+        limit: 6, // Giới hạn số lượng kết quả trả về thành 6
+      });
+    }
+    res.json(cities);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Lỗi truy vấn cơ sở dữ liệu' });
+  }
+});
+
+
+
 
 module.exports = router;
