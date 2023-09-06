ae24d180895ba8d842990f980d4e03231e31a987
Trần Văn Lộc <tranvanloc96lhp@gmail.com>
2023-09-06 17:31:28 +0700

    up

--- controllers/homestayController.js
+++ controllers/homestayController.js
@@ -6,7 +6,7 @@ exports.getHomeStay = async (req, res, next) => {
     const page = 'homestay'; 
     try {
         const cities = await City.findAll();
-        res.render('homestay', { page: page, title: 'Khách sạn', cities }); // Truyền dữ liệu vào template HBS
+        res.render('homestay', { page: page, title: 'Khách sạn', cities, homestay: 'active' }); // Truyền dữ liệu vào template HBS
     } catch (error) {
         console.error('Lỗi truy vấn:', error);
         res.render('error', { error: error.message });
