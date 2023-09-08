5145672706ab4970582fa070ee48d91a9026a933
Trần Văn Lộc <tranvanloc96lhp@gmail.com>
2023-09-08 11:47:18 +0700

    up

--- controllers/userController.js
+++ controllers/userController.js
@@ -8,23 +8,30 @@ require('../config/passport-config'); // Import cấu hình Passport.js
 exports.login = (req, res, next) => {
     passport.authenticate('local', {
         // successRedirect: '/', // Điều hướng sau khi đăng nhập thành công
-        // failureRedirect: '/homestay', // Điều hướng nếu đăng nhập thất bại
+        failureRedirect: '/', // Điều hướng nếu đăng nhập thất bại
         failureFlash: true, // Cho phép thông báo lỗi
     })(req, res, () => {
         // Đăng nhập thành công, bạn có thể truy cập thông tin người dùng từ req.user
         const user = req.user;
-
         // Truyền thông tin người dùng vào session (nếu bạn sử dụng session)
         req.session.user = user;
-
-        // Hoặc truyền thông tin người dùng qua query string
-        // res.redirect(`/?username=${user.username}&email=${user.email}`);
-
-        // Sau khi truyền thông tin người dùng, thực hiện chuyển hướng về /
         res.redirect('/');
     });
 };
 
+exports.logout = (req, res) => {
+    // Xóa thông tin người dùng khỏi session (nếu bạn sử dụng session)
+    req.session.destroy((err) => {
+        if (err) {
+            console.error('Lỗi khi đăng xuất:', err);
+        }
+        // Sau khi đăng xuất, bạn có thể chuyển hướng người dùng đến trang chủ hoặc bất kỳ trang nào khác
+        res.redirect('/');
+    });
+};
+
+
 module.exports = {
     login: exports.login,
+    logout: exports.logout,
 };
--- routes/users.js
+++ routes/users.js
@@ -3,5 +3,7 @@ const router = express.Router();
 const userController = require('../controllers/userController');
 
 router.post('/dangnhap', userController.login);
+router.get('/dangxuat', userController.logout);
+
 
 module.exports = router;
